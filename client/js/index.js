13db06f2f85ca07546cca7970dbb5fa0f99a71ce
Vlad <vladislavnekhaichik@gmail.com>
2021-04-19 00:41:11 +0300

    added tilemap, optimized sprite, minor changes

--- client/js/index.js
+++ client/js/index.js
@@ -13,11 +13,12 @@ var config = {
 };
 
 var consts = {
-    playerSpeed: 200,
+    playerSpeed: 100,
     animFrameRate: 12,
     spriteFrame: 16
 };
 
+var map;
 var game = new Phaser.Game(config);
 
 var cursors;
@@ -25,57 +26,64 @@ var player;
 
 function preload () {
     this.load.spritesheet('sprite', './art/sprite.png', { frameWidth: consts.spriteFrame, frameHeight: consts.spriteFrame });
+    this.load.tilemapCSV('map', './maps/demo01.csv');
 }
 
 function create () {
+    map = this.add.tilemap('map', consts.spriteFrame, consts.spriteFrame);
+    var tileset = map.addTilesetImage('sprite');
+    var layer = map.createLayer(0, tileset, 0, 0);
+    layer.skipCull = true;
+
     cursors = this.input.keyboard.createCursorKeys();
 
     setupBomb = setupBomb.bind(this);
     spawnBomb = spawnBomb.bind(this);
     setupPlayer = setupPlayer.bind(this);
     spawnPlayer = spawnPlayer.bind(this);
+    alignToWorld = alignToWorld.bind(this);
 
     setupBomb();
     setupPlayer();
 
-    player = spawnPlayer(400, 300);
-    spawnBomb(200, 300);
+    player = spawnPlayer(0, 0);
 }
 
 function update() {
-    player.setVelocity(0);
-    if (cursors.left.isDown) {
-        player.setVelocityX(-consts.playerSpeed);
-        player.anims.play('player-left', true);
+    player.gameObj.setVelocity(0);
+    if (cursors.space.isDown) {
+        player.placeBomb();
+    } else if (cursors.left.isDown) {
+        player.left();
     } else if (cursors.right.isDown) {
-        player.setVelocityX(consts.playerSpeed);
-        player.anims.play('player-right', true);
+        player.right();
     } else if (cursors.up.isDown) {
-        player.setVelocityY(-consts.playerSpeed);
-        player.anims.play('player-up', true);
+        player.up();
     } else if (cursors.down.isDown) {
-        player.setVelocityY(consts.playerSpeed);
-        player.anims.play('player-down', true);
+        player.down();
     } else {
-        player.anims.play('player-idle', true);
+        player.idle();
     }
 }
 
 function setupBomb() {
     this.anims.create({
         key: 'bomb',
-        frames: this.anims.generateFrameNumbers('sprite', { frames: [30, 30, 30, 31, 31] }),
+        frames: this.anims.generateFrameNumbers('sprite', { frames: [21, 21, 21, 22, 22] }),
         frameRate: consts.animFrameRate,
         repeat: -1
     });
 }
 
 function spawnBomb(x, y) {
-    var bomb = this.physics.add.sprite(x, y, 'sprite');
-    bomb.setCollideWorldBounds(true);
-    bomb.setScale(2);
-    bomb.play('bomb');
-    return bomb;
+    var offset = consts.spriteFrame / 2;
+    var gameObj = this.physics.add.sprite(x, y, 'sprite');
+    gameObj.setCollideWorldBounds(true);
+    gameObj.play('bomb');
+
+    return {
+        gameObj: gameObj
+    };
 }
 
 function setupPlayer() {
@@ -91,13 +99,13 @@ function setupPlayer() {
     });
     this.anims.create({
         key: 'player-right',
-        frames: this.anims.generateFrameNumbers('sprite', { frames: [10, 11, 12] }),
+        frames: this.anims.generateFrameNumbers('sprite', { frames: [7, 8, 9] }),
         frameRate: consts.animFrameRate,
         repeat: -1
     });
     this.anims.create({
         key: 'player-up',
-        frames: this.anims.generateFrameNumbers('sprite', { frames: [13, 14, 15] }),
+        frames: this.anims.generateFrameNumbers('sprite', { frames: [10, 11, 12] }),
         frameRate: consts.animFrameRate,
         repeat: -1
     });
@@ -110,9 +118,41 @@ function setupPlayer() {
 }
 
 function spawnPlayer(x, y) {
-    var player = this.physics.add.sprite(x, y, 'sprite');
-    player.setCollideWorldBounds(true);
-    player.setScale(2);
-    player.play('player-idle');
-    return player;
+    var gameObj = this.physics.add.sprite(x, y, 'sprite');
+    gameObj.setCollideWorldBounds(true);
+    gameObj.play('player-idle');
+    
+    return {
+        gameObj: gameObj,
+        left: function() {
+            gameObj.setVelocityX(-consts.playerSpeed);
+            gameObj.anims.play('player-left', true);
+        },
+        right: function() {
+            gameObj.setVelocityX(consts.playerSpeed);
+            gameObj.anims.play('player-right', true);
+        },
+        up: function() {
+            gameObj.setVelocityY(-consts.playerSpeed);
+            gameObj.anims.play('player-up', true);
+        },
+        down: function() {
+            gameObj.setVelocityY(consts.playerSpeed);
+            gameObj.anims.play('player-down', true);
+        },
+        idle: function() {
+            gameObj.anims.play('player-idle', true);
+        },
+        placeBomb: function() {
+            var crd = alignToWorld(gameObj.x, gameObj.y);
+            spawnBomb(crd.x, crd.y);
+        }
+    };
+}
+
+function alignToWorld(x, y) {
+    return {
+        x: Phaser.Math.Snap.Floor(x, consts.spriteFrame),
+        y: Phaser.Math.Snap.Floor(y, consts.spriteFrame)
+    };
 }
